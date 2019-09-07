bbd8871593b28b9a20c41fcbb9d7922dca1da8e7
Jeremy O <galaxyjay123@gmail.com>
2019-09-06 18:35:31 -0600

    postman-working

---
+++ blogger-project/server/controllers/ValuesController.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import ValuesService from '../services/ValuesService';
+import { Authorize } from '../middleware/authorize.js'
+
+let _valuesService = new ValuesService().repository
+
+export default class ValuesController {
+    constructor() {
+        this.router = express.Router()
+            //NOTE all routes after the authenticate method will require the user to be logged in to access
+            .use(Authorize.authenticated)
+            .get('', this.getAll)
+            .get('/:id', this.getById)
+            .post('', this.create)
+            .put('/:id', this.edit)
+            .delete('/:id', this.delete)
+    }
+
+    async getAll(req, res, next) {
+        try {
+            let data = await _valuesService.find({})
+            return res.send(data)
+        } catch (error) { next(error) }
+
+    }
+
+    async getById(req, res, next) {
+        try {
+            let data = await _valuesService.findById(req.params.id)
+            if (!data) {
+                throw new Error("Invalid Id")
+            }
+            res.send(data)
+        } catch (error) { next(error) }
+    }
+
+    async create(req, res, next) {
+        try {
+            //NOTE the user id is accessable through req.body.uid, never trust the client to provide you this information
+            req.body.authorId = req.session.uid
+            let data = await _valuesService.create(req.body)
+            res.send(data)
+        } catch (error) { next(error) }
+    }
+
+    async edit(req, res, next) {
+        try {
+            let data = await _valuesService.findOneAndUpdate({ _id: req.params.id, }, req.body, { new: true })
+            if (data) {
+                return res.send(data)
+            }
+            throw new Error("invalid id")
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            await _valuesService.findOneAndRemove({ _id: req.params.id })
+            res.send("deleted values")
+        } catch (error) { next(error) }
+
+    }
+
+}
